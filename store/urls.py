5aa3ac15ccd7ac9669d70d89ba14e7f6457fa5f5
thami <thamielmoumeni@gmail.com>
2023-05-22 21:51:27 +0100

    fixed urls error

--- store/urls.py
+++ store/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-
 from . import views
+
+
+app_name = 'adoption'
+
 
 urlpatterns = [
 
