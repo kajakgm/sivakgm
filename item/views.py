504e756385bcf18493d255602940a2054147b1c5
thami <thamielmoumeni@gmail.com>
2023-05-25 20:12:25 +0100

    doing goood

--- item/views.py
+++ item/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Item, Category
-from .forms import ProductFilterForm
+import json
 from django.views.generic import ListView
 
 
 def items(request):
+    query = request.GET.get('query','')
     items = Item.objects.filter(is_sold=False)
+
+    if query:
+        items = items.filter(name__icontains=query)
 
     return render(request, 'item/items.html', {
         'items':  items,
+        'query': query,
     })
+
+
 
 def item_list(request):
     items = Item.objects.all()
-    category = ProductFilterForm
+    
     context = {'items': items,
-                'Category': category}
+                }
     return render(request, 'store.html', context)
 
 
