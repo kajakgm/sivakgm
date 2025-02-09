4a0e1b4cfd523a7e4987c97aa2fbab7ae8363e8f
unknown <kajakgm@gmail.com>
2025-02-09 15:58:05 +0530

    first commit

--- adoption/settings.py
+++ adoption/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'adoption.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'store', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
